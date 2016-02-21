cd419d3d17f06ce4a6cfa107ba6a7f6c4a613c18
Bruce Burgess <bruceburgess67@yahoo.com>
2016-02-21 23:07:02 +0700

    Store color and moe to next view controller

--- Color Fantasic/ViewController.swift
+++ Color Fantasic/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     //Connects the tableview from the storyboard to code.
     @IBOutlet weak var tableView: UITableView!
     
+    //This variable will take the selected color from the user and store it
+    var selectedColor : UIColor?
+    
     //Setting colors
     let colors : [UIColor] = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.purpleColor(), UIColor(colorLiteralRed: 255/255, green: 0/255, blue: 128/255, alpha: 1.0), UIColor.blackColor()]
     
@@ -23,6 +26,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //Connecting the delegate and data source to this View Controller
         self.tableView.dataSource = self
         self.tableView.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,11 +52,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     //This function is called when the user selects a row. It goes to the color view controller
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        print("table row select")
+        //Storing the select color to pass to the next view
+        self.selectedColor = self.colors[indexPath.row]
         //Unselects the selected cell
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        //tableView.deselectRowAtIndexPath(indexPath, animated: true)
         //Performes the transition using the segue to next view controller
         self.performSegueWithIdentifier("colorSegue", sender: self)
+    }
+    
+    //This function executes just before transition to other view controller
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "colorSegue" {
+            //Make the background color of the view from what the user selected
+            segue.destinationViewController.view.backgroundColor = self.selectedColor
+        }
     }
 
 
